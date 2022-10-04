16dc6f2c5f1a6a7ab710f3213bb2973f54df06b1
Thecoder0012 <mardineliassi57@gmail.com>
2022-10-04 11:50:02 +0200

    Initial commit

---
+++ src/test/java/com/example/adventure/AdventureApplicationTests.java
@@ -0,0 +1,10 @@
+package com.example.adventure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdventureApplicationTests {
+
+    @Test
+    void contextLoads() {
