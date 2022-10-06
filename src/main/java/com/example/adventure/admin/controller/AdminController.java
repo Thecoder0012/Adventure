3b92c1c74e592123251d82319961d0c4738ad533
moha <moha729g@stud.kea.dk>
2022-10-06 11:21:45 +0200

    AdminController init

---
+++ src/main/java/com/example/adventure/admin/controller/AdminController.java
@@ -0,0 +1,27 @@
+package com.example.adventure.admin.controller;
+
+import com.example.adventure.booking.model.Booking;
+import com.example.adventure.booking.service.BookingService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("api/v1/admin")
+@CrossOrigin
+public class AdminController {
+
+    private final BookingService bookingService;
+
+    @GetMapping("/bookings")
+    public ResponseEntity<List<Booking>> fetchAll(){
+        return ResponseEntity.ok().body(bookingService.getAll());
+    }
+}
+
