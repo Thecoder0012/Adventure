a5e5e4699a63e296cbd786163920e6b7ba586e78
radwan <radw0001@stud.dk>
2022-10-07 12:45:12 +0200

    dto part booking

--- src/main/java/com/example/adventure/dtotest/DtoFactory.java
+++ src/main/java/com/example/adventure/dtotest/DtoFactory.java
@@ -1,7 +1,11 @@
 package com.example.adventure.dtotest;
 
 import com.example.adventure.activity.model.Activity;
+import com.example.adventure.booking.model.Booking;
+import com.example.adventure.customer.model.Customer;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -23,4 +27,24 @@ public class DtoFactory {
         return activities.stream().map(obj -> fromActivity(obj))
                 .collect(Collectors.toList());
     }
-}
+
+    public static BookingDto fromBooking(Booking booking) {
+        BookingDto dto = new BookingDto();
+        dto.setId(booking.getId());
+        dto.setLocalDate(booking.getLocalDate());
+        dto.setTimeEnd(booking.getTimeEnd());
+        dto.setTimeStart(booking.getTimeStart());
+        dto.setCustomer(booking.getCustomer());
+        dto.setActivity(booking.getActivity());
+        return dto;
+    }
+
+    public static List<BookingDto> fromBookings(List<Booking> bookings) {
+        return bookings.stream().map(obj -> fromBooking(obj))
+                .collect(Collectors.toList());
+    }
+
+    }
+
+
+
